77da0e76e2dd986c6e659f2fd9afaca09618b116
gdmec07150708 <151415992@qq.com>
2016-11-20 18:29:03 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/s07150708/mycontacts/ContactsMessageActivity.java
@@ -0,0 +1,56 @@
+package cn.edu.gdmec.s07150708.mycontacts;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+/**
+ * Created by 123 on 2016/11/20.
+ */
+public class ContactsMessageActivity extends AppCompatActivity{
+    private EditText nameEditText;
+    private EditText mobileEditText;
+    private EditText qqEditText;
+    private EditText danweiEditText;
+    private EditText addressEditText;
+    private User user;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts_message);
+        nameEditText=(EditText) findViewById(R.id.name);
+        mobileEditText=(EditText) findViewById(R.id.mobile);
+        danweiEditText= (EditText) findViewById(R.id.danwei);
+        qqEditText= (EditText) findViewById(R.id.qq);
+        addressEditText= (EditText) findViewById(R.id.address);
+        Bundle localBundle=getIntent().getExtras();
+        int id=localBundle.getInt("user_ID");
+        ContactsTable contactsTable=new ContactsTable(this);
+        user =contactsTable.getUserByID(id);
+        nameEditText.setText("姓名："+user.getName());
+        mobileEditText.setText("电话："+user.getMobile());
+        qqEditText.setText("qq："+user.getQq());
+        danweiEditText.setText("单位："+user.getDanwei());
+        addressEditText.setText("地址："+user.getAddress());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0,1,0,"返回");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case 1:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
